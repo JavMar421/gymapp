bf3322582e339bf3312d2843b699d895c4b61be6
Javier <javmar421@aul.centroafuera.es>
2022-05-13 02:39:34 +0200

    13/5

--- app/src/main/java/com/example/gymapp/MainActivity.kt
+++ app/src/main/java/com/example/gymapp/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.example.gymapp
 
+//import ActivityMainViewModel
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.recyclerview.widget.RecyclerView
+import com.example.gymapp.databinding.ActivityMainBinding
+import com.example.gymapp.databinding.ActivityRegisterBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import androidx.activity.viewModels
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    //private val viewModel: ActivityMainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(3000)
+            print("...iniciando login...")
+            setContentView(R.layout.activity_login)
+
+        }
+
+
     }
+
 }
