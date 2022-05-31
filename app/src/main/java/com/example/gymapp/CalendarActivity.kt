f9892e6848de8c1feb7d3b651360c5511f4f4dc4
Javier <javmar421@aul.centroafuera.es>
2022-05-31 18:17:46 +0200

    esquinas redondeadas

--- app/src/main/java/com/example/gymapp/CalendarActivity.kt
+++ app/src/main/java/com/example/gymapp/CalendarActivity.kt
@@ -3,10 +3,13 @@ package com.example.gymapp
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CalendarView
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.get
 
 class CalendarActivity : AppCompatActivity() {
         @RequiresApi(Build.VERSION_CODES.M)
@@ -16,7 +19,10 @@ class CalendarActivity : AppCompatActivity() {
             val logout = findViewById<ImageView>(R.id.logoutlogo)
             val edittable = findViewById<ImageView>(R.id.tablaicon)
             val editejer = findViewById<ImageView>(R.id.ejericon)
-            val calendar = findViewById<ImageView>(R.id.calendaricon)
+            val calendaricon = findViewById<ImageView>(R.id.calendaricon)
+            val calendar = findViewById<CalendarView>(R.id.calendar)
+            val siguiente = findViewById<Button>(R.id.Siguiente)
+            val anterior = findViewById<Button>(R.id.Anterior)
             logout.setOnClickListener{
                 Toast.makeText(this,"$saveuser ha Cerrado la Sesión", Toast.LENGTH_SHORT).show()
                 val intent = Intent(this, MainActivity::class.java)
@@ -30,10 +36,24 @@ class CalendarActivity : AppCompatActivity() {
                 val intent = Intent(this, EjerCreator::class.java)
                 startActivity(intent)
             }
-            calendar.setOnClickListener{
+            calendaricon.setOnClickListener{
                 val intent = Intent(this, CalendarActivity::class.java)
                 startActivity(intent)
             }
+            
+            siguiente.setOnClickListener {
+                var cosa=""
+
+                Toast.makeText(this,cosa,Toast.LENGTH_SHORT).show()
+
+            }
+            anterior.setOnClickListener {
+                var cosa=""
+                Toast.makeText(this,cosa,Toast.LENGTH_SHORT).show()
+
+            }
+
+
 
         }
     }
